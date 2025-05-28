57b584658df0d2a2803e3d6a72124153e66fe044
Deinabo Mayaki <deinabomayaki@gmail.com>
2025-05-28 19:24:23 +0000

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,6 @@ import {RouterProvider} from "react-router-dom";
 
 import {appRouter} from "@/constants/router";
 
-import { useHotjar } from './hooks/useHotjar';
-
-function App() {
-  useHotjar();
-
-  return (
-    <>
-      {/* Your App content */}
-    </>
-  );
-}
-
-export default App;
-
 
 export default function MainApp() { 
   return <RouterProvider router={appRouter} />
