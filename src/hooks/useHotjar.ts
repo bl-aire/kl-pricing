36590ee0c25a6f6cde7876b0fd7c27f9ce4d48ea
Deinabo Mayaki <deinabomayaki@gmail.com>
2025-05-28 18:56:39 +0000

    New tag for the Hotjar script

--- src/hooks/useHotjar.ts
+++ src/hooks/useHotjar.ts
@@ -7,18 +7,14 @@ export const useHotjar = () => {
     if (process.env.NODE_ENV !== 'production') return;
     if (window.hj) return; // Prevent double-inject
 
+    <!-- Hotjar Tracking Code for Kasuwa Landing -->
     (function(h,o,t,j,a,r){
-      h.hj =
-        h.hj ||
-        function() {
-          (h.hj.q = h.hj.q || []).push(arguments);
-        };
-      h._hjSettings = { hjid: 3901981, hjsv: 6 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script');
-      r.async = 1;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6418579,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
   }, []);
 };
