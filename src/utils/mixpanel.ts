5b772b6227132e2ed3c70035aecad725c52446ed
bl_aire <ogoh_blessing@yahoo.com>
2025-10-18 13:30:13 +0100

    -ve script

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -7,13 +7,4 @@ mixpanel.init(MIXPANEL_TOKEN, {
   debug: process.env.NODE_ENV !== 'production',
 });
 
-/*declare module '@/utils/mixpanel' {
-  const mixpanel: {
-    track(event: string, props?: Record<string, any>): void;
-    identify?(id: string): void;
-    [key: string]: any;
-  };
-  export default mixpanel;
-}*/
-
 export default mixpanel;
